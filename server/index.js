e40f473e6dc6ec5b3dbc3b80a4c92543b6f4a82b
Caio Vieira <caioviier@gmail.com>
2023-05-23 20:00:34 -0300

    fix: alterações na criação do servidor

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const configExpress = require('./app/config/express')
-const config = require('./app/config/general')
+const configExpress = require('./app/config/express.cfg')
+const config = require('./app/config/general.cfg')
 
 const app = express()
 
-configExpress(app)
+// configExpress(app)
 
-app.listen(config().app.port)
+app.listen(config().app.port, () => {
+	console.log('listening on port ' + config().app.port)
+})
